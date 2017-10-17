d8fbf47603c52c81858725098706272d18fb984b
Ahmad Gneady <support@bigprof.com>
2017-10-17 15:46:24 +0200

    Bug fix in ajax-invoice-subtotal.php when invoice has no items yet. Added description of tax field.

--- hooks/ajax-invoice-subtotal.php
+++ hooks/ajax-invoice-subtotal.php
@@ -15,8 +15,14 @@
 	if(!$id) exit;
 	
 	$subtotal = sqlValue("select sum(price) from invoice_items where invoice='{$id}'");
+	if(!$subtotal) $subtotal = 0.0;
+	
 	$tax = sqlValue("select tax from invoices where id='{$id}'");
+	if(!$tax) $tax = 0.0;
+	
 	$discount = sqlValue("select discount from invoices where id='{$id}'");
+	if(!$discount) $discount = 0.0;
+	
 	$total = $subtotal * (1 - $discount / 100) * (1 + $tax / 100);
 	
 	sql("update invoices set subtotal='{$subtotal}', total='{$total}' where id='{$id}'",$eo);
