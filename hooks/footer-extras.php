f7e1b32f8c93f3b9cda727a5a7b3991cc0d7bc19
Ahmad Gneady <support@bigprof.com>
2017-10-17 17:02:20 +0200

    Enhanced homepage appearance

--- hooks/footer-extras.php
+++ hooks/footer-extras.php
@@ -1,7 +1,13 @@
 <style>
-	.stat{
-		height: initial !important;
+	.btn.stat {
+		height: 8em !important;
+		max-width: 20em;
+		padding: .75em;
+		margin-right: .75em;
+	}	
+	.btn.stat .badge {
+		display: block;
+		font-size: 4em;
+		margin-bottom: .1em;
 	}
 </style>
-
-<?php
--- hooks/links-home.php
+++ hooks/links-home.php
@@ -15,39 +15,42 @@
 	 */
 
 
-$year = date("Y");
-$month = date("m");
+	$year = date("Y");
+	$month = date("m");
 
-$sql = "SELECT COUNT(id) FROM invoices WHERE status='Unpaid' AND MONTH(date_due)='%s' AND YEAR(date_due)='%s'";
+	$sql = "SELECT COUNT(id) FROM invoices WHERE status='Unpaid' AND MONTH(date_due)='%s' AND YEAR(date_due)='%s'";
+	$due = sqlValue(sprintf($sql, $month, $year), $error); // due
+	$upcoming = sqlValue(sprintf($sql, ++$month, $year), $error); // due
 
+	$homeLinks[] = array(
+		'url' => 'hooks/invoice_button.php', 
+		'title' => 'Reports', 
+		'description' => '<div id="reports-section"></div>',
+		'groups' => array('*'), // groups allowed to see this link, use '*' if you want to show the link to all groups
+		'grid_column_classes' => 'col-md-12', // optional CSS classes to apply to link block. See: http://getbootstrap.com/css/#grid
+		'panel_classes' => 'panel-info', // optional CSS classes to apply to panel. See: http://getbootstrap.com/components/#panels
+		'link_classes' => 'btn-info', // optional CSS classes to apply to link. See: http://getbootstrap.com/css/#buttons
+		'icon' => 'resources/table_icons/attributes_display.png' // optional icon to use with the link
+	);
 
-$due = sqlValue(sprintf($sql, $month, $year), $error); // due
-
-$upcoming = sqlValue(sprintf($sql, ++$month, $year), $error); // due
-
-ob_start();
 ?>
 
+<div id="reports-draft" class="hidden">
+	<a class="btn btn-warning stat" href="hooks/due_invoices.php">
+		<span class="badge"><?php echo $due; ?></span>
+		Due invoices 
+	</a>
+	<a class="stat btn btn-info" href="hooks/upcoming_invoices.php">
+		<span class='badge'><?php echo $upcoming;?></span>
+		Upcoming invoices 
+	</a> 
+</div>
 
-<h5>Smart Reports for your work</h5>
-<a class="stat btn btn-info" href="hooks/upcoming_invoices.php">Upcoming invoices 
-	<span class='badge'><?php echo $upcoming;?></span>
-</a> 
-<a class="btn btn-danger stat" href="hooks/due_invoices.php">Due invoices 
-	<span class="badge"><?php echo $due; ?></span>
-</a>
+<script>
+	$j(function(){
+		$j('#reports-section').html($j('#reports-draft').html());
+		$j('#reports-draft').remove();
+	})
+</script>
 
-<?php
 
-$description = ob_get_clean();
-$homeLinks[] = array(
-			'url' => 'hooks/invoice_button.php', 
-			'title' => 'Reports', 
-			'description' => $description,
-                     
-			'groups' => array('*'), // groups allowed to see this link, use '*' if you want to show the link to all groups
-			'grid_column_classes' => 'col-md-12', // optional CSS classes to apply to link block. See: http://getbootstrap.com/css/#grid
-			'panel_classes' => 'panel-success', // optional CSS classes to apply to panel. See: http://getbootstrap.com/components/#panels
-			'link_classes' => 'btn-success', // optional CSS classes to apply to link. See: http://getbootstrap.com/css/#buttons
-			'icon' => '' // optional icon to use with the link
-		);
