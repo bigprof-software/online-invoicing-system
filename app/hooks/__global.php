74732c60cb539d5563682b5083cf3d4754f44d39
Ahmad Gneady <support@bigprof.com>
2020-11-25 20:25:25 +0200

    Remove obsolete functions no longer needed after updating to AppGini latest release.

--- app/hooks/__global.php
+++ app/hooks/__global.php
@@ -105,8 +105,6 @@
 
 	}
 
-/*
-
 	function print_invoice_query($id){
 		$eo = ['silentErrors' => true];
 		$query = sql("
@@ -147,64 +145,7 @@
 
 		return $results;
 	}
-
-	function update_item_latest_price($item_id) {
-		$item_id = max(0, intval($item_id));
-		$last_price = get_item_price($item_id);
-		
-		$eo = ['silentErrors' => true];
-		sql("UPDATE `items` SET `unit_price`='{$last_price}' WHERE `id`='{$item_id}'", $eo);
-		
-		return $last_price;
-	}
 	
-	function get_item_price($item_id, $date = false) {
-		if($date === false) $date = date('Y-m-d');
-		$item_id = max(0, intval($item_id));
-
-		$last_price = sqlValue("SELECT `price` FROM `item_prices` WHERE `item`='{$item_id}' AND `date`<='{$date}' ORDER BY `date` DESC, `id` DESC LIMIT 1");
-		
-		return $last_price ? $last_price : sqlValue("SELECT `unit_price` FROM `items` WHERE `id`='{$item_id}'");
-	}
-
-*/
-	/* retrieve all products as an array of assoc. arrays, cached */
-/*
-	function get_products() {
-		static $products = false;
-		if($products !== false) return $products;
-		
-		$products = [];
-		$res = sql("SELECT * FROM `products` ORDER BY `id`", $eo);
-		while($row = db_fetch_assoc($res)) $products[] = $row;
-		
-		return $products;
-	}
-*/
-	
-	/* 
-		takes array like this:
-		[
-			['koko' => 123, 'lolo' => 'abc'],
-			['koko' => 245, 'lolo' => 'xyz'],
-			['koko' => 667, 'lolo' => 'wer']
-		]
-		and returns array like this (if @param key is 'koko'):
-		[123, 245, 667]
-	*/
-/*
-	function array_flatten($arr, $key){
-		if(!is_array($arr)) return array();
-		
-		$fa = array(); // flat array to return
-		foreach($arr as $sa){
-			if(!isset($sa[$key])) continue;
-			$fa[] = $sa[$key];
-		}
-		
-		return $fa;
-	}
-
 	function convertNumberToWord($num = false) {
 		$num = str_replace([',', ' '], '', trim($num));
 		if(!$num) return false;
@@ -252,29 +193,4 @@
 
 		return implode(' ', $words) . ($fractions ? " and {$fractions}" : '');
 	}
-*/
 
-	/**
-	 *  @return HTML code for report action buttons (back, print, .. etc)
-	 */
-/*
-	function report_actions(){
-		global $Translation;
-		
-		ob_start();
-		?>
-		<a href="reports.php" class="btn btn-info hidden-print btn-lg" role="button"><i class="glyphicon glyphicon-chevron-left"></i> <?php echo $Translation['Back']; ?></a>
-		<button class="btn btn-primary hidden-print btn-lg hspacer-lg" type="button" id="sendToPrinter" onclick="window.print();"><i class="glyphicon glyphicon-print"></i> <?php echo $Translation['Print']; ?></button>
-		<?php
-		
-		return ob_get_clean();
-	}
-	
-	function restrict_access($table = 'invoices'){
-		$from_chk = get_sql_from($table);
-		if(!$from_chk){
-			header("HTTP/1.0 401 Unauthorized");
-			exit;
-		}
-	}
-*/
