561c0fdaa6ad51d1ef46e66a36f6a9809e8235cf
Ahmad Gneady <support@bigprof.com>
2021-07-01 21:21:09 +0200

    Fixed stored XSS issue.

--- app/invoices_autofill.php
+++ app/invoices_autofill.php
@@ -37,12 +37,12 @@
 			$res = sql("SELECT `clients`.`id` as 'id', `clients`.`name` as 'name', `clients`.`contact` as 'contact', `clients`.`title` as 'title', `clients`.`address` as 'address', `clients`.`city` as 'city', `clients`.`country` as 'country', CONCAT_WS('-', LEFT(`clients`.`phone`,3), MID(`clients`.`phone`,4,3), RIGHT(`clients`.`phone`,4)) as 'phone', `clients`.`email` as 'email', `clients`.`website` as 'website', `clients`.`comments` as 'comments', `clients`.`unpaid_sales` as 'unpaid_sales', `clients`.`paid_sales` as 'paid_sales', `clients`.`total_sales` as 'total_sales' FROM `clients`  WHERE `clients`.`id`='{$id}' limit 1", $eo);
 			$row = db_fetch_assoc($res);
 			?>
-			$j('#client_contact<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', nl2br($row['contact']))); ?>&nbsp;');
-			$j('#client_address<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', nl2br($row['address']))); ?>&nbsp;');
-			$j('#client_phone<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', nl2br($row['phone']))); ?>&nbsp;');
-			$j('#client_email<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', nl2br($row['email']))); ?>&nbsp;');
-			$j('#client_website<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', nl2br($row['website']))); ?>&nbsp;');
-			$j('#client_comments<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', nl2br($row['comments']))); ?>&nbsp;');
+			$j('#client_contact<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', safe_html($row['contact']))); ?>&nbsp;');
+			$j('#client_address<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', safe_html($row['address']))); ?>&nbsp;');
+			$j('#client_phone<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', safe_html($row['phone']))); ?>&nbsp;');
+			$j('#client_email<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', safe_html($row['email']))); ?>&nbsp;');
+			$j('#client_website<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', safe_html($row['website']))); ?>&nbsp;');
+			$j('#client_comments<?php echo $rnd1; ?>').html('<?php echo addslashes(str_replace(array("\r", "\n"), '', safe_html($row['comments']))); ?>&nbsp;');
 			<?php
 			break;
 
