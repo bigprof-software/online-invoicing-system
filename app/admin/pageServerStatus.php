c644e869417b356f1fea956ba55604066aa9576a
Ahmad Gneady <support@bigprof.com>
2021-07-11 01:51:12 +0200

    Fix low severity stored xss in batch transfer wizard.

--- app/admin/pageServerStatus.php
+++ app/admin/pageServerStatus.php
@@ -1,6 +1,6 @@
 <?php
 	$appgini_version = '6.0.1145';
-	$generated_ts = '10/7/2021 9:20:39 PM';
+	$generated_ts = '11/7/2021 1:49:55 AM';
 
 	$currDir = dirname(__FILE__);
 	require("{$currDir}/incCommon.php");
